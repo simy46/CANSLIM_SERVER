d0ca29a077d1f184635f789cb7b0bc5c5af97b10
simy46 <missipsa.annane@polymtl.ca>
2024-06-06 16:58:40 -0400

    wirteOnFile

--- services/fileManager.js
+++ services/fileManager.js
@@ -1,28 +1,26 @@
 import fs from 'fs';
 import path from 'path';
 
+
 export async function writeOnFile(message) {
     const logFilePath = path.resolve('./logs/logs.txt');
     const dir = path.dirname(logFilePath);
 
-    console.log(logFilePath)
-    console.log(dir)
+    // Ensure the directory exists
+    fs.mkdir(dir, { recursive: true }, (err) => {
+        if (err) {
+            console.error('Error creating directory:', err);
+            return;
+        }
 
-    return new Promise((resolve, reject) => {
-        // Ensure the directory exists
-        fs.mkdir(dir, { recursive: true }, (err) => {
+        // Write to the file
+        fs.writeFile(logFilePath, message, { flag: 'a' }, (err) => { // 'a' flag for appending
             if (err) {
-                return reject(err);
+                console.error('Error writing to file:', err);
+            } else {
+                console.log('Successfully written to file:', message);
             }
-
-            // Write to the file
-            fs.appendFile(logFilePath, message, (err) => {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve();
-                }
-            });
         });
     });
 }
+
